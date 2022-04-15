bf29b8cac53ad936925e2af9c84fa70423f78fcc
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-04-15 12:08:44 -0300

    create playRound function, rework computerPlay function to work with playRound

--- gamescript.js
+++ gamescript.js
@@ -1,7 +1,14 @@
 const selection = ["Rock", "Paper", "Scissors"];
+const random = Math.floor(Math.random() * selection.length);
 
-function computerPlay(selection) {
-  return selection[Math.floor(Math.random() * selection.length)];
+function computerPlay() {
+  return random, selection[random];
 }
 
-console.log(computerPlay(selection));
+const playerSelection = window.prompt("Make a selection");
+
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection + " " + computerPlay());
+}
+
+playRound(playerSelection);
