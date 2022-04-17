daae2a476aa3be884adac331ad4f5b4c190742cb
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-04-17 13:39:57 -0300

    creat function to determine winner and loser

--- gamescript.js
+++ gamescript.js
@@ -4,44 +4,52 @@ function computerPlay() {
   const random = selection[Math.floor(Math.random() * selection.length)];
   return random;
 }
-
 // plays a single round between player and computer
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     tie++;
-    return `You picked ${playerSelection} and your opponent picked ${computerSelection}. It's a tie!`;
+    return console.log(
+      `You picked ${playerSelection} and your opponent picked ${computerSelection}. It's a tie!`
+    );
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
     playerScore++;
-    return "Rock beats paper.You win!";
+    return console.log("Rock beats paper.You win!");
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
     playerScore++;
-    return "Rock beats paper. You win!";
+    return console.log("Rock beats paper. You win!");
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
     playerScore++;
-    return "Rock beats paper. You win!";
+    return console.log("Rock beats paper. You win!");
   } else {
     computerScore++;
-    return `${computerSelection} beats ${playerSelection}. You lose!`;
+    return console.log(
+      `${computerSelection} beats ${playerSelection}. You lose!`
+    );
   }
 }
-//plays 5 rounds of the game
+//stores score count
 let tie = 0;
 let playerScore = 0;
 let computerScore = 0;
-
+// plays five rounds
 function game() {
   for (let i = 0; i < 5; i++) {
     let playerSelection = window.prompt("Make a selection");
     let computerSelection = computerPlay();
 
-    console.log(`"Computer plays"${computerSelection}`);
+    console.log(`Computer chooses ${computerSelection}.`);
 
     playRound(playerSelection, computerSelection);
   }
 }
-
+// determines winner and loser
 function result() {
   if (tie > playerScore && tie > computerScore) {
+    console.log("It's a tie!");
+  } else if (playerScore > computerScore) {
+    console.log("You win the game!");
+  } else {
+    console.log("You lose the game!");
   }
 }
-console.log(game());
+game();
