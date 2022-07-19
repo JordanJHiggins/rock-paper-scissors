914e0b6638c6e036ae107a6f88622cafe86a3ef9
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-07-19 19:36:09 -0300

    Rework comments and change code format

--- gamescript.js
+++ gamescript.js
@@ -1,37 +1,43 @@
-// generates a random choice for computer opponent
+// Game
+let tie = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   const selection = ["Rock", "Paper", "Scissors"];
   const random = selection[Math.floor(Math.random() * selection.length)];
   return random;
 }
-// plays a single round between player and computer
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     tie++;
-    return console.log(
+    console.log(
       `You picked ${playerSelection} and your opponent picked ${computerSelection}. It's a tie!`
     );
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    playerScore++;
-    return console.log("Rock beats paper.You win!");
+    playerScore = playerScore++;
+    console.log("Rock beats paper.You win!");
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    playerScore++;
-    return console.log("Rock beats paper. You win!");
+    playerScore = playerScore++;
+    console.log("Rock beats paper. You win!");
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    playerScore++;
-    return console.log("Rock beats paper. You win!");
+    playerScore = playerScore++;
+    console.log("Rock beats paper. You win!");
   } else {
-    computerScore++;
-    return console.log(
-      `${computerSelection} beats ${playerSelection}. You lose!`
-    );
+    computerScore = computerScore++;
+    console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
   }
 }
-//stores score count
-let tie = 0;
-let playerScore = 0;
-let computerScore = 0;
-// plays five rounds
+
+function result() {
+  if (playerScore === 5) {
+    console.log("You win the game!");
+  } else if (computerScore === 5) {
+    console.log("You lose the game!");
+  }
+}
+
 /*
 function game() {
   for (let i = 0; i < 5; i++) {
@@ -44,19 +50,8 @@ function game() {
   }
 }
 */
-// determines winner and loser
-function result() {
-  if (tie > playerScore && tie > computerScore) {
-    console.log("It's a tie!");
-  } else if (playerScore > computerScore) {
-    console.log("You win the game!");
-  } else {
-    console.log("You lose the game!");
-  }
-}
-// game();
 
-// creates three buttons
+// GUI
 function createButtons() {
   const rockButton = document.createElement("button");
   const paperButton = document.createElement("button");
@@ -84,12 +79,3 @@ function createButtons() {
 }
 
 createButtons();
-
-// tracks score
-function scoreBoard(result) {
-  if (result === "You win the game!") {
-    playerScore++;
-  } else if (result === "You lose the game!") {
-    computerScore++;
-  }
-}
